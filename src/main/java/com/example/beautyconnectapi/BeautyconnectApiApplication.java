fa9004e7e446adffcf7b416c88f355a3d59aa067
Candela Puerta <109047221+candelabp@users.noreply.github.com>
2025-07-09 17:06:39 -0300

    initial commit

---
+++ src/main/java/com/example/beautyconnectapi/BeautyconnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.beautyconnectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyconnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyconnectApiApplication.class, args);
+	}
+
+}
