ab2e2e36b93d2705de6e969a04c632f0212326df
flopigimenez <fgimenezmorcos@gmail.com>
2025-07-21 12:28:30 -0300

    centro controller

--- src/main/java/com/example/beautyconnectapi/controller/CentroDeEsteticaController.java
+++ src/main/java/com/example/beautyconnectapi/controller/CentroDeEsteticaController.java
@@ -1,13 +1,43 @@
 package com.example.beautyconnectapi.controller;
 
+import com.example.beautyconnectapi.model.dto.centroDeEstetica.CentroDeEsteticaDTO;
+import com.example.beautyconnectapi.model.dto.centroDeEstetica.CentroDeEsteticaResponseDTO;
+import com.example.beautyconnectapi.model.entity.CentroDeEstetica;
+import com.example.beautyconnectapi.model.enums.Estado;
 import com.example.beautyconnectapi.service.CentroDeEsteticaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/centrodeestetica")
 public class CentroDeEsteticaController {
     @Autowired
     private CentroDeEsteticaService centroDeEsteticaService;
+
+    public CentroDeEsteticaController(CentroDeEsteticaService centroDeEsteticaService) {
+        this.centroDeEsteticaService = centroDeEsteticaService;
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity<CentroDeEsteticaResponseDTO> registrar(@RequestBody CentroDeEsteticaDTO centroDeEsteticaDto) {
+        return ResponseEntity.ok(centroDeEsteticaService.registrar(centroDeEsteticaDto));
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<CentroDeEsteticaResponseDTO>> listar() {
+        return ResponseEntity.ok(centroDeEsteticaService.listar());
+    }
+
+    @GetMapping("/listarEstado")
+    public ResponseEntity<List<CentroDeEsteticaResponseDTO>> listarPorEstado(Estado estado) {
+        return ResponseEntity.ok(centroDeEsteticaService.listarPorEstado(estado));
+    }
+
+    @PutMapping("/cambiarEstado")
+    public ResponseEntity<CentroDeEsteticaResponseDTO> cambiarEstado(Long id, Estado estado) {
+        return ResponseEntity.ok(centroDeEsteticaService.cambiarEstado(id, estado));
+    }
 }
