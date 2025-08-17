4a2d0c001ef4fcd5882020c1aaf5b89ad17ba9e3
Candela Puerta <109047221+candelabp@users.noreply.github.com>
2025-08-17 20:41:39 -0300

    cloudinary-back

---
+++ src/main/java/com/example/beautyconnectapi/service/ImageService.java
@@ -0,0 +1,16 @@
+package com.example.beautyconnectapi.service;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public interface ImageService {
+    ResponseEntity<List<Map<String, Object>>> getAllImages();
+    ResponseEntity<String> uploadImages(MultipartFile[] files);
+    ResponseEntity<String> deleteImage(String publicId, UUID uuid);
+
+
+}
