cb6fd8b194c37ecb6424de111abfd92e92fa775e
flopigimenez <fgimenezmorcos@gmail.com>
2025-10-05 21:16:18 -0300

    .

--- src/main/java/com/example/beautyconnectapi/config/MailConfig.java
+++ src/main/java/com/example/beautyconnectapi/config/MailConfig.java
@@ -1,5 +1,6 @@
 package com.example.beautyconnectapi.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,13 +11,19 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.username}")
+    private String mailUser;
+
+    @Value("${spring.mail.password}")
+    private String mailPassword;
+
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("beautyconnect16@gmail.com");
-        mailSender.setPassword("fusk iifw cpcm vxwq");
+        mailSender.setUsername(mailUser);
+        mailSender.setPassword(mailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
