50187846cd5eb3f2286475df3e3ec1bfc9524d17
Candela Puerta <109047221+candelabp@users.noreply.github.com>
2025-10-09 21:40:52 -0300

    ..

--- src/main/java/com/example/beautyconnectapi/repository/HorarioCentroRepository.java
+++ src/main/java/com/example/beautyconnectapi/repository/HorarioCentroRepository.java
@@ -2,19 +2,9 @@ package com.example.beautyconnectapi.repository;
 
 import com.example.beautyconnectapi.model.entity.HorarioCentro;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface HorarioCentroRepository extends JpaRepository<HorarioCentro, Long> {
-    List<HorarioCentro> findByCentroDeEsteticaId(Long centroId);
-
-    @Modifying
-    @Query("DELETE FROM HorarioCentro h WHERE h.centroDeEstetica.id = :centroId")
-    void deleteByCentroDeEsteticaId(@Param("centroId") Long centroId);
 
 }
